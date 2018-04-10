b036a50376184aa952d4f435871962e4371fdcb0
pawarsagar <pawar.sagar875.sp@gmail.com>
2018-04-10 23:57:52 +0530

    querystring is done

---
+++ src/components/FetchURL.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { parse } from 'qs';
+import queryString from 'query-string' // install this package 
+
+const query = queryString.parse(location.hash.substr(2))   
+console.log("id is "+ query.Cid+" token is: "+query.token)   ///             install package  and variable is query and use dot to fetch token and id 
+/* 
+const List = ({ location }) => {
+    const query = queryString.parse(location.hash.substr(2))
+   
+    console.log(location);
+    return (
+        <div>
+            <p>CID: {query.Cid}</p>
+            <p>Sid: {query.token}</p>
+        </div>
+    )
+} */
+//export default List

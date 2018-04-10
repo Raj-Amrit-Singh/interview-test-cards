8fcff7f0fa84481b5f25d11ece0f75d9e67612c6
Raj-Amrit-Singh <aj170896@gmail.com>
2018-04-10 23:09:18 +0530

    removed the bug related to options

--- src/components/PresentQuestions.js
+++ src/components/PresentQuestions.js
@@ -27,9 +27,9 @@ export class PresentQuestions extends React.Component {
     for (let i = 1; i <= 20; i++) {
       this.buttonElements.push(
         <div className="btn-group">
-          
-        <button  className="btn btn-primary" id={i} key={i} onClick={() => this.selectQuestion(i)} className="btn btn-primary" >{i}</button>
-        &nbsp;
+
+          <button className="btn btn-primary" id={i} key={i} onClick={() => this.selectQuestion(i)} className="btn btn-primary" >{i}</button>
+          &nbsp;
         </div>
       )
     }
@@ -62,14 +62,20 @@ export class PresentQuestions extends React.Component {
   }
 
   onButtonChange(value, id, answer) {
-    console.log("button change");
     let tempArray = this.state.currentButton;
+    let group = Math.floor(value / 4);
+    let removeNumber = tempArray.find((element, index) => {
+      return Math.floor(element / 4) == group;
+    });
+    let index = tempArray.indexOf(removeNumber);
+    if (index > -1) {
+      tempArray.splice(index, 1);
+    }
     tempArray.push(value);
     this.setState((prevState) => ({
       currentButton: tempArray,
     }));
     this.answerStore(id, answer);
-    console.log(this.state.currentButton);
   }
 
   componentWillMount() {
@@ -77,15 +83,13 @@ export class PresentQuestions extends React.Component {
   }
 
   toCheck(value) {
-      let checkResult;
-      let tempArray = this.state.currentButton;
-      console.log(this.state.currentButton);
-      if ((tempArray.indexOf(value)) >= 0) {
-        checkResult = true;
-      }
-      else checkResult = false;
-      console.log(checkResult);
-      return checkResult;      
+    let checkResult;
+    let tempArray = this.state.currentButton;
+    if ((tempArray.indexOf(value)) >= 0) {
+      checkResult = true;
+    }
+    else checkResult = false;
+    return checkResult;
   }
 
   render() {
@@ -94,51 +98,52 @@ export class PresentQuestions extends React.Component {
     let currentStatus = this.qBank[tempState];
     return (
       <div className="row margin-40 " >
-      <div className="col-sm-12 col-sm-offset-1 text-absolute" style={{
-    top:"-16px"}}>
-        <h1 className="white text-center">Aptitude Test</h1>
-        <hr ></hr>
-      <div className="panel-group well well-lg">
-     {/*  <div className="panel panel-default" > */}
-        <div className="panel-heading " >Qusetion - " {currentStatus.Question__c } "</div>
-        <div className="panel-body" >
-          <div className="radio">
-          <table> 
-            <tr>
-            <td><label><input type="radio" style={{position: "inherit"}} name={tempState} value={myValue + 1} checked={this.toCheck(myValue + 1)} onChange={() => this.onButtonChange(myValue + 1, currentStatus.Id, currentStatus.Options__c.option1)} />
-             &nbsp; {currentStatus.Options__c.option1}
-            </label></td>
+        <div className="col-sm-12 col-sm-offset-1 text-absolute" style={{
+          top: "-16px"
+        }}>
+          <h1 className="white text-center">Aptitude Test</h1>
+          <hr ></hr>
+          <div className="panel-group well well-lg">
+            {/*  <div className="panel panel-default" > */}
+            <div className="panel-heading " >Qusetion - " {currentStatus.Question__c} "</div>
+            <div className="panel-body" >
+              <div className="radio">
+                <table>
+                  <tr>
+                    <td><label><input type="radio" style={{ position: "inherit" }} name={tempState} value={myValue} checked={this.toCheck(myValue)} onChange={() => this.onButtonChange(myValue, currentStatus.Id, currentStatus.Options__c.option1)} />
+                      &nbsp; {currentStatus.Options__c.option1}
+                    </label></td>
 
-            <td><label style={{ marginLeft: "387px" }}>
-              <input type="radio" style={{position: "inherit"}} name={tempState} value={myValue + 2} checked={this.toCheck(myValue + 2)} onChange={() => this.onButtonChange(myValue + 2, currentStatus.Id, currentStatus.Options__c.option2)} />
-              &nbsp; {currentStatus.Options__c.option2}
-            </label></td></tr>
-            <tr> <td><label><input type="radio" style={{position: "inherit"}} name={tempState} value={myValue + 3} checked={this.toCheck(myValue + 3)} onChange={() => this.onButtonChange(myValue + 3, currentStatus.Id, currentStatus.Options__c.option3)} />
-            &nbsp; {currentStatus.Options__c.option3}
-            </label></td>
-            <td><label style={{ marginLeft: "387px" }}>
-              <input type="radio" name={tempState} style={{position: "inherit"}} value={myValue + 4} checked={this.toCheck(myValue + 4)} onChange={() => this.onButtonChange(myValue + 4, currentStatus.Id, currentStatus.Options__c.option4)} />
-              &nbsp; {currentStatus.Options__c.option4}
-            </label></td></tr>
-            </table>
-          </div>
-        </div>
-        <hr className="hrLine"></hr>
-        <div className="btn-group " >
-        &nbsp;
+                    <td><label style={{ marginLeft: "387px" }}>
+                      <input type="radio" style={{ position: "inherit" }} name={tempState} value={myValue + 1} checked={this.toCheck(myValue + 1)} onChange={() => this.onButtonChange(myValue + 1, currentStatus.Id, currentStatus.Options__c.option2)} />
+                      &nbsp; {currentStatus.Options__c.option2}
+                    </label></td></tr>
+                  <tr> <td><label><input type="radio" style={{ position: "inherit" }} name={tempState} value={myValue + 2} checked={this.toCheck(myValue + 2)} onChange={() => this.onButtonChange(myValue + 2, currentStatus.Id, currentStatus.Options__c.option3)} />
+                    &nbsp; {currentStatus.Options__c.option3}
+                  </label></td>
+                    <td><label style={{ marginLeft: "387px" }}>
+                      <input type="radio" name={tempState} style={{ position: "inherit" }} value={myValue + 3} checked={this.toCheck(myValue + 3)} onChange={() => this.onButtonChange(myValue + 3, currentStatus.Id, currentStatus.Options__c.option4)} />
+                      &nbsp; {currentStatus.Options__c.option4}
+                    </label></td></tr>
+                </table>
+              </div>
+            </div>
+            <hr className="hrLine"></hr>
+            <div className="btn-group " >
+              &nbsp;
         <span className="inline">
 
-          {this.buttonElements}
-        </span>
-        </div>
-        &nbsp;
+                {this.buttonElements}
+              </span>
+            </div>
+            &nbsp;
           <button className="previous round" onClick={this.prevQuestion}>Prev</button>
-          <button className="next round"  onClick={this.nextQuestion}>Next</button>
-         
+            <button className="next round" onClick={this.nextQuestion}>Next</button>
+
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
-    /*   </div> */
+      /*   </div> */
     );
   }
 }
